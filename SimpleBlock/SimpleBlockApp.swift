67208183aad5fc34f72b70a54c7b15d174cbdeb6
phasefactor <56488589+phasefactor@users.noreply.github.com>
2023-10-12 12:07:32 -0500

    Initial commit.  Bare minimum working example.

---
+++ SimpleBlock/SimpleBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBlockApp.swift
+//  SimpleBlock
+//
+//  Created by user on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
