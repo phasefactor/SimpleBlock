6bd07f839ad958be3fb67bb86c1043b8fb96dc17
phasefactor <56488589+phasefactor@users.noreply.github.com>
2023-10-15 19:49:11 -0500

    Initial (and very ugly) attempt to block YouTube ads.

--- SimpleBlockExtension/Resources/content.js
+++ SimpleBlockExtension/Resources/content.js
@@ -1,7 +1,22 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+/*
+ 
+ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+ console.log("Received response: ", response);
+ });
+ 
+ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+ console.log("Received request: ", request);
+ });
+ 
+ */
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+// this is an affront to decency, but we have little control over timing
+(function(i) {
+    if (i) {
+        document.body.querySelectorAll("div.ytp-ad-skip-button-text").forEach( (el) => {el.click();} );
+        setTimeout(() => {arguments.callee(--i);}, 100);
+    } else {
+        document.body.querySelectorAll("div.ytp-ad-module").forEach( (el) => {el.remove();} );
+    }
+})(50);
+
