0baa3e11286812feeed410c158c798bfbbe080f8
phasefactor <56488589+phasefactor@users.noreply.github.com>
2023-10-30 07:53:12 -0500

    Moved YouTube domain blocking rules to separate file.  Need to add a filter to them.

--- SimpleBlockExtension/Resources/youtube.js
+++ SimpleBlockExtension/Resources/youtube.js
@@ -1,17 +1,5 @@
-
-// this is an affront to decency, but we have little control over timing
-/*
-(function(i) {
-    if (i) {
-        document.body.querySelectorAll("div.ytp-ad-skip-button-text").forEach( (el) => {el.click();} );
-        setTimeout(() => {arguments.callee(--i);}, 100);
-    } else {
-        document.body.querySelectorAll("div.ytp-ad-module").forEach( (el) => {el.remove();} );
-    }
-})(50);
-*/
-
-
+//
+// Inject a script into the main window execution context
 //
 // Based on code from:
 // https://stackoverflow.com/questions/9515704/access-variables-and-functions-defined-in-page-context-using-a-content-script
@@ -20,7 +8,51 @@
 
 var actualCode = '(' + function() {
     console.log("content.js running");
-
+/*
+    _fetch = fetch;
+    let _vid = "";
+    let _loc = "";
+    
+    fetch = function () {
+        let url = "";
+        
+        if (arguments[0] instanceof Request) {
+            url = arguments[0].url;
+        } else {
+            url = arguments[0];
+        }
+        
+        
+        if (url.includes("googlevideo.com/videoplayback")) {
+            let params = new URLSearchParams(url);
+            
+            
+            let id = params.get("id");
+            
+            if (_loc != window.location.href) {
+                console.log("new location");
+                _loc = window.location.href;
+                _vid = "";
+            }
+            
+            if (id != null) {
+                if (_vid == "") {
+                    _vid = id;
+                    console.log("new id");
+                }
+                
+                if (_vid != id) {
+               //     arguments = [];
+                    console.log("dropping fetch for ID:", id);
+                } else {
+                    console.log("fetching ID:", id);
+                }
+            }
+        }
+        
+        return _fetch.apply(this, arguments);
+    }
+*/
     
 } + ')();';
 
@@ -30,22 +62,5 @@ script.textContent = actualCode;
 script.remove();
 
 /*
- 
- {"id":1000,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/error_204.*",}},
- {"id":1001,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/generate_204.*",}},
- {"id":1002,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?googlevideo\\.com/generate_204.*",}},
- {"id":1003,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/pagead/.*",}},
- {"id":1004,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/api/stats/ad.*",}},
- {"id":1005,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/api/stats/qoe.*",}},
- {"id":1006,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/api/stats/atr.*",}},
- {"id":1007,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/api/stats/watchtime.*",}},
- {"id":1008,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/api/stats/delayplay.*",}},
- {"id":1009,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/api/stats/playback.*",}},
- {"id":1010,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/ptracking.*",}},
- {"id":1017,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/youtubei/v1/log_even.*",}},
- {"id":1018,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?play\\.google\\.com/log.*",}},
- {"id":1019,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?google\\.com/pagead/.*",}},
- {"id":1020,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?youtube\\.com/pcs/activeview.*",}},
- {"id":1001,"priority":1,"action":{"type":"block"},"condition":{"regexFilter":"http(s)?://(.*\\.)?ytimg\\.com/generate_204.*",}},
  
  */
